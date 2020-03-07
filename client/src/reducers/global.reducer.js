7705ebede82c2ab2338dd3e9e2d58ae50df998a2
gebhartn <nicholas.gebhart@gmail.com>
2020-03-07 14:05:41 -0800

    chore: reducer scaffolding

--- client/src/reducers/global.reducer.js
+++ client/src/reducers/global.reducer.js
@@ -1,13 +1,28 @@
 export const globalState = {
   title: 'Hello World',
+  fetching: false,
+  error: '',
 };
 
 export const globalReducer = (state = globalState, action) => {
   switch (action.type) {
-    case 'TEST':
+    case 'FETCH_SUCCESS':
       return {
         ...state,
         title: action.payload,
+      };
+
+    case 'FETCH_PENDING':
+      return {
+        ...state,
+        fetching: true,
+      };
+
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload,
       };
 
     default:
