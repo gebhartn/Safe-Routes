8afa26e972f7a7619f0f3c9092c525562973bdaf
gebhartn <nicholas.gebhart@gmail.com>
2020-03-07 16:50:12 -0800

    chore: updates landing

--- client/src/components/Landing/index.jsx
+++ client/src/components/Landing/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import Navigation from './Navigation';
 
 const Landing = () => (
   <>
-    <Navigation />
+    <div>Landing page</div>
   </>
 );
 export default Landing;
